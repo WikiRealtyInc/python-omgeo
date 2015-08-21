e03f5c4886567e5f5746f0b02be38493e50fec83
Harold Martin <harold.martin@gmail.com>
2015-08-21 14:52:22 -0700

    enable single term query to map quest

--- omgeo/services/__init__.py
+++ omgeo/services/__init__.py
@@ -197,21 +197,23 @@ class MapQuest(GeocodeService):
                 if kwargs[kw] != '':
                     location = dict(location, **{kw: kwargs[kw]})
             return location
-        location = {}
-        location = get_appended_location(location, street=pq.query)
-        if location == {}:
-            location = get_appended_location(location, street=pq.address)
-        location = get_appended_location(location, city=pq.city, county=pq.subregion, state=pq.state,
-                                         postalCode=pq.postal, country=pq.country)
-        json_ = dict(location=location)
-        json_ = json.dumps(json_)
-        logger.debug('MQ json: %s', json_)
-        query = dict(key=unquote(self._settings['api_key']),
-                     json=json_)
+        if pq.address.strip() != '':
+            location = {}
+            location = get_appended_location(location, street=pq.query)
+            if location == {}:
+                location = get_appended_location(location, street=pq.address)
+            location = get_appended_location(location, city=pq.city, county=pq.subregion, state=pq.state,
+                                             postalCode=pq.postal, country=pq.country)
+            json_ = dict(location=location)
+            json_ = json.dumps(json_)
+            query = dict(key=unquote(self._settings['api_key']),
+                         json=json_)
+        else:
+            query = dict(key=unquote(self._settings['api_key']),
+                     location=pq.query)
         if pq.viewbox is not None:
             query = dict(query, viewbox=pq.viewbox.to_mapquest_str())
         response_obj = self._get_json_obj(self._endpoint, query)
-        logger.debug('MQ RESPONSE: %s', response_obj)
         returned_candidates = [] # this will be the list returned
         for r in response_obj['results'][0]['locations']:
             c = Candidate()
